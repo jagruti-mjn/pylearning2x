9d9fcfb16b02d0560fd1cefbabd7bda560b2f947
jagruti-mjn <147322439+jagruti-mjn@users.noreply.github.com>
2023-10-11 14:02:29 +0530

    9 oct code

---
+++ 9 OCT/lab0019.py
@@ -0,0 +1,48 @@
+#string
+name = "jagruti"
+print(name)
+
+#string functions
+#capitalize
+result =name.capitalize()
+print(result)
+
+print(id(name))
+print(id(result))
+
+#upper case charecter
+result2 =name.upper()
+print(result2)
+
+#lower case charecter
+result3 = name.lower()
+print(result3)
+
+#swapcase charecter
+result4 = name.swapcase()
+print(result4)
+
+#title case
+
+name = "hello jagruti"
+print (name.title())
+
+#replace case
+text = "hello jagruti"
+result_rep = text.replace("jagruti","amol")
+print(result_rep)
+
+#index & lenght
+name = "jagruti"
+print(len(name))
+print(name[1])
+
+
+#find
+name= "hello jagruti"
+index= name.find("jagruti")
+print(index)
+
+#count
+count=text.count("l")
+
